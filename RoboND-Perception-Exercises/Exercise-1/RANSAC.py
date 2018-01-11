c35da62551264f2b28fdd4c0b1d6c3107fe7dd22
Kapil Gupta <kpgupta98@gmail.com>
2018-01-11 22:01:28 +0530

    exercise 1 complete

--- RoboND-Perception-Exercises/Exercise-1/RANSAC.py
+++ RoboND-Perception-Exercises/Exercise-1/RANSAC.py
@@ -5,24 +5,58 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
-# Voxel Grid filter
+## Voxel Grid filter
+vox = cloud.make_voxel_grid_filter()
+LEAF_SIZE = 0.01
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+cloud_filtered = vox.filter()
+
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 
-# PassThrough filter
+## PassThrough filter
+passthrough = cloud_filtered.make_passthrough_filter()
+
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+
+pcl.save(cloud_filtered, filename)
 
 
-# RANSAC plane segmentation
+## RANSAC plane segmentation
+seg = cloud_filtered.make_segmenter()
+
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+inliers, coefficients = seg.segment()
 
 
-# Extract inliers
+## Extract inliers
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+filename = 'extracted_inliers.pcd'
 
 # Save pcd for table
 # pcl.save(cloud, filename)
+pcl.save(extracted_inliers, filename)
 
 
-# Extract outliers
+## Extract outliers
+extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_outliers.pcd'
 
 
-# Save pcd for tabletop objects
-
-
+## Save pcd for tabletop objects
+pcl.save(extracted_outliers, filename)
